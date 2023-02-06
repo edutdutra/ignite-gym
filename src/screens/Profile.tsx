0ee4d3c777b1133fcf254320ba5a618e65936501
eduardodutra <edutdutra@gmail.com>
2023-02-06 12:33:28 -0300

    add source app

---
+++ src/screens/Profile.tsx
@@ -0,0 +1,122 @@
+import {useState} from "react";
+
+import {Alert, TouchableOpacity} from "react-native";
+import {Center, ScrollView, Skeleton, VStack, Text, Heading, useToast} from "native-base";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+
+
+import {ScreenHeader} from "@components/ScreenHeader";
+import {UserPhoto} from "@components/UserPhoto";
+import {Input} from "@components/Input";
+import {Button} from "@components/Button";
+
+const PHOTO_SIZE = 33;
+
+export function Profile() {
+    const [photoIsLoading, setPhotoIsLoading] = useState(false);
+    const [userPhoto, setUserPhoto] = useState('https://github.com/edutdutra.png');
+
+    const toast = useToast();
+
+    async function handleUserPhotoSelect() {
+        setPhotoIsLoading(true);
+        try {
+            const photoSelected = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+                aspect: [4, 4],
+                allowsEditing: true,
+            });
+
+            if (photoSelected.canceled) {
+                return;
+            }
+
+            if (photoSelected.assets[0].uri) {
+                const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+
+                if (photoInfo.size && (photoInfo.size /1024 /1024) > 5) {
+                    return toast.show({
+                        title: 'Essa imagem é muito grande. Escolha uma até 5MB',
+                        placement: 'top',
+                        bgColor: 'red.500'
+                    });
+                }
+
+                setUserPhoto(photoSelected.assets[0].uri);
+            }
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setPhotoIsLoading(false);
+        }
+    }
+
+    return (
+        <VStack flex={1}>
+            <ScreenHeader title="Perfil"/>
+            <ScrollView contentContainerStyle={{paddingBottom: 36}}>
+                <Center mt={6} px={10}>
+                    {
+                        photoIsLoading ?
+                            <Skeleton
+                                w={PHOTO_SIZE}
+                                h={PHOTO_SIZE}
+                                rounded="full"
+                                startColor="gray.500"
+                                endColor="gray.400"
+                            />
+                            :
+                            <UserPhoto
+                                source={{uri: userPhoto}}
+                                alt="Foto do usuário"
+                                size={PHOTO_SIZE}
+                            />
+                    }
+
+                    <TouchableOpacity onPress={handleUserPhotoSelect}>
+                        <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+                            Alterar foto
+                        </Text>
+                    </TouchableOpacity>
+
+                    <Input
+                        placeholder="Nome"
+                        bg="gray.600"
+                    />
+                    <Input
+                        placeholder="E-mail"
+                        bg="gray.600"
+                        isDisabled
+                    />
+                    <Heading color="gray.200" fontSize="md" fontFamily="heading" mb={2} alignSelf="flex-start" mt={12}>
+                        Alterar senha
+                    </Heading>
+
+                    <Input
+                        placeholder="Senha antiga"
+                        bg="gray.600"
+                        secureTextEntry
+                    />
+                    <Input
+                        placeholder="Nova senha"
+                        bg="gray.600"
+                        secureTextEntry
+                    />
+                    <Input
+                        placeholder="Confirme nova senha"
+                        bg="gray.600"
+                        secureTextEntry
+                    />
+
+                    <Button
+                        title="Atualizar"
+                        mt={4}
+                    />
+                </Center>
+            </ScrollView>
+        </VStack>
+    )
+}
