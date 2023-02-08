e1bceaaed0f79c721ed3b5e405aa94b7f771232f
eduardodutra <edutdutra@gmail.com>
2023-02-08 09:23:01 -0300

    add axios and error tratament

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,4 @@
-import {VStack, Image, Text, Center, Heading, ScrollView} from "native-base";
+import {VStack, Image, Text, Center, Heading, ScrollView, useToast} from "native-base";
 import {useNavigation} from "@react-navigation/native";
 import {Controller, useForm} from "react-hook-form";
 import * as yup from 'yup';
@@ -9,6 +9,9 @@ import {Button} from "@components/Button";
 
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
+
+import {api} from "@services/api";
+import {AppError} from "@utils/AppError";
 
 type FormDataProps = {
     name: string;
@@ -29,13 +32,26 @@ export function SignUp() {
         resolver: yupResolver(signUpSchema)
     });
     const navigation = useNavigation();
+    const toast = useToast();
 
     function handleGoBack() {
         navigation.goBack();
     }
 
-    function handleSignUp({name, email, password, password_confirm}: FormDataProps) {
-        console.log({name, email, password, password_confirm});
+    async function handleSignUp({name, email, password}: FormDataProps) {
+        try {
+            const response = await api.post('/users', {name, email, password});
+            console.log(response.data);
+        } catch (error) {
+            const isAppError = error instanceof AppError
+            const title = isAppError ? error.message : 'Não foi possivel criar a conta. Tente novamente mais tarde';
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        }
     }
 
     return (
